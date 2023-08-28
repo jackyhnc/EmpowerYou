1eab556491cf174e37b5481e1c548b55ea03f3eb
jackkhc <hancjacky@gmail.com>
2023-08-28 18:49:28 -0400

    main page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,13 +12,13 @@ function App() {
             <img className="first-section__graphic" src="../womenholdingfistsupgraphic.png"></img>
             <div className="first-section__main">
                 <div className="first-section__title">Igniting Change Through Education and Equality</div>
-                <div className="first-section__text">EmpowerYou is more than just a learning hub; it's a transformative platform dedicated to fostering gender equality and empowering women worldwide. Our mission is to amplify the voices of women's rights movements at the local level, provide vital resources through accessible hotlines, facilitate a supportive network for abuse victims to connect and heal, and prioritize daily mental health checkups to ensure holistic well-being.</div>
+                <div className="first-section__text">EmpowerYou is more than just a learning hub; it's a transformative platform dedicated to fostering gender equality and empowering women worldwide.</div>
                 <div className="first-section__button">Learn More</div>
             </div>
         </div>
         <div className="sec-section">
             <div className="sec-section__left">
-
+                <img className="sec-section__graphic" src="../graphicwomenhackathon.png"></img>
             </div>
             <div className="sec-section__right">
                 <div className="sec-section__main">
@@ -30,9 +30,52 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div className="sec-section__left">
-                <img className="sec-section__graphic" src="../graphicwomenhackathon.png"></img>
+
+        </div>
+        <div className="third-fourth-section-container">
+            <div className="third-section">
+                <div className="third-section__title">How we can help!</div>
+                <div className="third-section__text">In Afghanistan, South Sudan, Chad, Iran, Syria, (where domestic abuse cases are rampant) and beyond, we are driven by a shared vision: to provide unwavering support to women facing adversity. Our mission transcends borders, as we amplify the voices of survivors and champion their rights to safety and dignity. By offering helplines, resources, and networks, we're building bridges to hope.</div>
+                <div className="third-section__graphics-container">
+                    <div className="third-section__icons-text-container">
+                        <img className="third-section__icon" src="../mental-health.png"></img>
+                        <div className="third-third-section__icons-text-container__text">Frequent mental health checkups</div>
+                    </div>
+                    <div className="third-section__icons-text-container">
+                        <img className="third-section__icon" src="../mortarboard.png"></img>
+                        <div className="third-third-section__icons-text-container__text">Lessons on dealing with adversities & local movements</div>
+                    </div>
+                    <div className="third-section__icons-text-container">
+                        <img className="third-section__icon" src="../growth.png"></img>
+                        <div className="third-third-section__icons-text-container__text">Ever-lasting personal growth</div>
+                    </div>
+                </div>
             </div>
+            <div className="fourth-section">
+                <div className="fourth-section__left">
+                    <img className="fourth-section__graphic" src="../womenprotest.png"></img>
+                </div>
+                <div className="fourth-section__right">
+                    <div className="fourth-section__main">
+                        <div className="fourth-section__title">Goal of Unity</div>
+                        <div className="fourth-section__text">
+                            Through collaboration with local organizations, NGOs, and global partners, EmpowerYou strives to empower women to reclaim their narratives. We recognize that progress is a collective endeavor, and our mission underscores the power of unity.
+                            <br/>
+                            <br/>
+                            Embark on a journey towards personal growth, collective progress, and impactful change.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fifth-section">
+                <div className="fifth-section__title">Make a Difference Today</div>
+                <div className="first-section__button">Join the Movement</div>
+
+            </div>
+
+            <footer>
+
+            </footer>
         </div>
 
     </>
--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react'
 import './navbar.css'
 
 export default function Navbar() {
+    const [navShadow, setNavShadow] = useState(false)
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 0) {
+            setNavShadow(true)
+        }
+        else {
+            setNavShadow(false)
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
     return (
         <nav>
+            <div id="nav-shadow-id" className={navShadow ? `nav-shadow-true` : "nav-shadow-false"}></div>
             <img className="nav__logo" src="../empoweryoulogo.png"></img>
             <div className="nav__buttons-container">
-                <div className="nav__buttons">Home</div>
-                <div className="nav__buttons">Login</div>
-                <div className="nav__buttons">Signup</div>
                 <div className="nav__buttons">Support</div>
                 <div className="nav__buttons">About</div>
+                <p>|</p>
+                <div className="nav__buttons">Login</div>
+                <div className="nav__buttons-special">Get Started</div>
+
             </div>
         </nav>
     )
